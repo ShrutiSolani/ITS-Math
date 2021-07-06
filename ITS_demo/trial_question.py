ad1e5f4319186b4814b0b266d467ba03a3d8f312
ShrutiSolani <shrutiusolani@gmail.com>
2021-07-06 20:38:42 +0530

    Problem solved

--- ITS_demo/trial_question.py
+++ ITS_demo/trial_question.py
@@ -550,5 +550,7 @@ def divide_whole():
     context={'que':que,'numerator':numerator,'hints':hints,'denominator':denominator,'ans_num':ans_num,'ans_den':ans_den,'rec_num':1,'rec_den':div}
     scoredict = {'score': "", 'total': "", 'totalqts': "", 'pct': ""}
     return render_template('divideby_whole.html',answer=context,scoredict=scoredict,hints=hints)
+
+
 app.secret_key = 'super secret key'
 app.run(debug=True)
