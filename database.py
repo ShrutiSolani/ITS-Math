521f39d053335fdffea6af37a2179ec4de1d53d5
ShrutiSolani <shrutiusolani@gmail.com>
2021-12-19 09:07:51 +0530

    db

--- database.py
+++ database.py
@@ -1,10 +1,16 @@
 import mysql.connector
 
-mydb = mysql.connector.connect(
-    host = "sql6.freesqldatabase.com",
-    user = "sql6449635",
-    database = "sql6449635",
-    password ="EH7dFtDVqR",
-    port = "3306"
-)
+# mydb = mysql.connector.connect(
+#     host = "sql6.freesqldatabase.com",
+#     user = "sql6449635",
+#     database = "sql6449635",
+#     password ="EH7dFtDVqR",
+#     port = "3306"
+# )
 
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="",
+    database="ITS"
+)
