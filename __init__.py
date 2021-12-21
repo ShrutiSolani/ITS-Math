2be8abd92a733235d960b5bfa6ceec469c7d824e
ShrutiSolani <shrutiusolani@gmail.com>
2021-12-21 19:43:50 +0530

    all changes

--- __init__.py
+++ __init__.py
@@ -1,46 +1,17 @@
 from flask import Flask
-import os
-import mysql.connector, datetime, logging, sys
 from .home.routes import home_bp
 from .fractions1.routes import fractions1_bp
 from .questions_list.routes import choice_bp
 from .algebra.routes import algebra_bp
-# from .home.routes import page_not_found
-# from pythonjsonlogger import jsonlogger
-
-from .database import mydb
-
+from decouple import config
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=False)
-
-    # logger = logging.getLogger()
-    # logHandler = logging.StreamHandler()
-    # formatter = jsonlogger.JsonFormatter()
-    # logHandler.setFormatter(formatter)
-    # logger.addHandler(logHandler)
-    if 'DYNO' in os.environ:
-        app.logger.addHandler(logging.StreamHandler(sys.stdout))
-        logging.basicConfig(filename = 'UserLog.log', level=logging.INFO, format = '%(asctime)s|%(message)s')
-    app.secret_key = 'super secret key'
-    log = logging.getLogger('werkzeug')
-    log.setLevel(logging.ERROR)
-    app.logger.addHandler(logging.StreamHandler(sys.stdout))
-    app.logger.setLevel(logging.ERROR)
+    app.secret_key = config('APP_SECRET_KEY')
     app.debug = True
-    # mydb = mysql.connector.connect(
-	#     host = "sql6.freesqldatabase.com",
-	#     user = "sql6449635",
-	#     database = "sql6449635",
-	#     password ="EH7dFtDVqR",
-	#     port = "3306"
-	# )
-
     app.register_blueprint(home_bp)
     app.register_blueprint(choice_bp)
     app.register_blueprint(fractions1_bp)
     app.register_blueprint(algebra_bp)
-    # app.register_error_handler(500,page_not_found)
-
     return app
---
+++ log.py
@@ -0,0 +1,30 @@
+from datetime import datetime
+
+from github import Github
+from github.InputGitAuthor import InputGitAuthor
+from decouple import config
+
+
+class Log:
+    def __init__(self):
+        pass
+
+    def log_entry(self, message):
+        try:
+            token = config('GITHUB_TOKEN')
+            file_path = config('LOG_FILE')
+            g = Github(token)
+            repo = g.get_repo(config('GITHUB_REPO'))
+            branch = config('GITHUB_BRANCH')
+            file = repo.get_contents(file_path, ref=branch)
+            data = file.decoded_content.decode("utf-8")
+            entry = str(datetime.now()) + '|' + message
+            data += "\n" + entry
+            author = InputGitAuthor(
+                config('GITHUB_USERNAME'),
+                config('GITHUB_EMAIL')
+            )
+            contents = repo.get_contents(file_path, ref=branch)
+            repo.update_file(contents.path, "log entry", data, contents.sha, branch=branch, author=author)
+        except Exception as e:
+            print(e)
